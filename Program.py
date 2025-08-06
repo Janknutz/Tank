619d5bcaab7a4b5bc0eb65aadf1e821a562d4651
Janknutz <152730316+Janknutz@users.noreply.github.com>
2025-08-06 15:05:32 -0400

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,7 +1,7 @@
 from superwires import games, color
 from flask import Flask
 import random, os, subprocess, pygame
-@app.route("/")
+
 app = Flask(__name__) 
 #app.run(host="0.0.0.0", port=10000)
 repo_root = os.path.dirname(os.path.abspath(__file__))
@@ -238,7 +238,7 @@ class instructions(games.Sprite):
         if games.keyboard.is_pressed(games.K_SPACE):
             self.destroy()
             main()
-        
+@app.route("/")       
 def main():
     
     #games.music.load("themey.mp3")
@@ -255,6 +255,7 @@ def main():
 instructions = instructions()
 games.screen.add(instructions)
 games.screen.mainloop()
+
 
 
 
